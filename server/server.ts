e35dec162e7187978e7985a83f500b13e2df72bf
Enzo <enzo@gmail.com>
2026-08-07 10:12:52 +0100

    layer of security added

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
 import authRouter from "./routes/authRoutes.js";
+import productRouter from "./routes/productRoutes.js";
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Server is Ready!');
 });
 app.use('/api/auth', authRouter)
+app.use('/api/products', productRouter)
 
 // Error Handle
 app.use((error: any, rereq: Request, res: Response, next: NextFunction) => {
