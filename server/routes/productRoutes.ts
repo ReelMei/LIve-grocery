46b2441878bedc08bd099a315caf592e2eceffc5
Enzo <enzo@gmail.com>
2026-08-07 11:42:53 +0100

    new

--- server/routes/productRoutes.ts
+++ server/routes/productRoutes.ts
@@ -12,7 +12,7 @@ productRouter.get("/", getProducts);
 productRouter.get("/:id", getProducts);
 productRouter.post("/", auth, admin, createProduct);
 productRouter.put("/:id", auth, admin, updateProduct);
-productRouter.delete("/:id", auth, admin, deleteProduct);
+
 
 
 
